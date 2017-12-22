93163d26f7b9dd3bc378dcea41caa2b7d8def8c5
Steven Leiva <leiva.steven@gmail.com>
2017-12-22 18:25:51 -0500

    Reset notifications when user complets a quiz.

--- QuizEnd.js
+++ QuizEnd.js
@@ -3,33 +3,45 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 import { connect } from "react-redux";
 
-import { centerAll, calculatePercent } from "./helpers";
+import {
+  centerAll,
+  calculatePercent,
+  clearLocalNotification,
+  setLocalNotification
+} from "./helpers";
 
-const QuizEnd = ({ correct, attempts, restartQuiz, backToDeck }) => {
-  return (
-    <View style={centerAll}>
-      <Text>
-        Correct: {correct}{" "}
-      </Text>
-      <Text>
-        Attempted: {attempts}
-      </Text>
-      <Text>
-        Score: {`${calculatePercent(correct, attempts)}%`}{" "}
-      </Text>
-      <Button
-        accessibilityLabel="Restart Quiz"
-        onPress={restartQuiz}
-        title="Restart Quiz"
-      />
+class QuizEnd extends React.Component {
+  componentDidMount() {
+    // user has completed a quiz
+    return clearLocalNotification().then(setLocalNotification);
+  }
 
-      <Button
-        accessibilityLabel="Back to Deck"
-        onPress={backToDeck}
-        title="Back to Deck"
-      />
-    </View>
-  );
-};
+  render() {
+    return (
+      <View style={centerAll}>
+        <Text>
+          Correct: {correct}{" "}
+        </Text>
+        <Text>
+          Attempted: {attempts}
+        </Text>
+        <Text>
+          Score: {`${calculatePercent(correct, attempts)}%`}{" "}
+        </Text>
+        <Button
+          accessibilityLabel="Restart Quiz"
+          onPress={restartQuiz}
+          title="Restart Quiz"
+        />
+
+        <Button
+          accessibilityLabel="Back to Deck"
+          onPress={backToDeck}
+          title="Back to Deck"
+        />
+      </View>
+    );
+  }
+}
 
 export default QuizEnd;
